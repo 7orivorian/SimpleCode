435099ab0da37f146836655bfdf8a069346f5009
7orivorian <7orivorian+github@gmail.com>
2024-06-24 19:38:31 -0400

    Initial commit

---
+++ src/main/java/dev/tori/simple/Main.java
@@ -0,0 +1,11 @@
+package dev.tori.simple;
+
+/**
+ * @author <a href="https://github.com/7orivorian">7orivorian</a>
+ * @since ${Since}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
